7130a401ae31251dbc109d4224c0c3c61068445a
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-04 13:29:26 +0300

    добавил блок try, except

--- pprint_json.py
+++ pprint_json.py
@@ -3,20 +3,17 @@ import sys
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except Exception:
-        print('File is empty')
-    finally:
-        exit()
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 
 def pretty_print_json(filepath):
-    return json.dumps(filepath, indent=4,
-                      sort_keys=True, ensure_ascii=False)
+    return json.dumps(filepath, indent=4, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    print(pretty_print_json(load_data(filepath)))
+    try:
+        filepath = sys.argv[1]
+        print(pretty_print_json(load_data(filepath)))
+    except (FileNotFoundError, ValueError):
+        print('Cannot read file')

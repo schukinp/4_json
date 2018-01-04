934bfa31216f08b130e9b689fe424e737edd3020
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-04 13:02:32 +0300

    изменил ввод файла с использованием sys

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,22 @@
 import json
-import argparse
-
-
-def get_file():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file')
-    return parser.parse_args().file
+import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return json.load(file)
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except Exception:
+        print('File is empty')
+    finally:
+        exit()
 
 
-def pretty_print_json(loaded_data):
-    return json.dumps(
-        loaded_data,
-        indent=4,
-        sort_keys=True,
-        ensure_ascii=False
-                      )
+def pretty_print_json(filepath):
+    return json.dumps(filepath, indent=4,
+                      sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    filepath = get_file()
-    loaded_data = load_data(filepath)
-    print(pretty_print_json(loaded_data))
+    filepath = sys.argv[1]
+    print(pretty_print_json(load_data(filepath)))
